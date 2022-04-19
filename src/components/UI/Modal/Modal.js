93557509fd6a1f529c0d8340e8ae95ef510db988
Pranav <naringrekarpranav6@live.com>
2022-04-20 03:45:55 +0530

    Added Portal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,44 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Card from '../Card/Card';
 import Button from '../Button/Button';
 import styles from './Modal.module.css';
 
+const Backdrop = (props) => (
+  <div className={styles.backdrop} onClick={props.onConfirm}></div>
+);
+
+const ModalOverlay = (props) => (
+  <Card className={styles.modal}>
+    <header className={styles.header}>
+      <h2>{props.title}</h2>
+    </header>
+    <div className={styles.content}>
+      <p>{props.message}</p>
+    </div>
+    <footer className={styles.actions}>
+      <Button onClick={props.onConfirm}>Okay</Button>
+    </footer>
+  </Card>
+);
+
 const Modal = (props) => {
   return (
-    <>
-      <div className={styles.backdrop} onClick={props.onConfirm}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById('modal-root')
+      )}
+    </React.Fragment>
   );
 };
 
